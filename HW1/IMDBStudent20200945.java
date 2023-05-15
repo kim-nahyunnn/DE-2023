3f55aa20879e04fd8a4cee955255a84a9592c858
kim-nahyunnn <nahyun8701@gmail.com>
2023-05-15 22:42:28 +0900

    uplaod

--- HW1/IMDBStudent20200945.java
+++ HW1/IMDBStudent20200945.java
@@ -1,1 +1,72 @@
-i
+import java.io.IOException;
+import java.util.*;
+
+import org.apache.hadoop.conf.*;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.*;
+import org.apache.hadoop.mapreduce.*;
+import org.apache.hadoop.mapreduce.lib.input.*;
+import org.apache.hadoop.mapreduce.lib.output.*;
+import org.apache.hadoop.util.GenericOptionsParser;
+
+public class IMDb
+{
+	public static class IMDbMapper extends Mapper<Object, Text, Text, IntWritable>
+	{
+		private final static IntWritable one = new IntWritable(1);
+		private Text word = new Text();
+
+		public void map(Object key, Text value, Context context) throws IOException, InterruptedException
+		{
+			StringTokenizer itr = new StringTokenizer(value.toString(), "::");
+			String genre;
+			while(itr.hasMoreTokens()){
+				genre = itr.nextToken();
+			}
+
+			itr = new StringTokenizer(genre, "|");
+			while(itr.hasMoreTokens()){
+				word.set(itr.nextToken());
+				context.write(word, one);
+			}
+		}
+	}
+
+	public static class IMDbReducer extends Reducer<Text, IntWritable, text, IntWritable>
+	{
+		private IntWritable result = new IntWritable();
+
+		public void reduce(Text key, Iterable<IntWritable> values, Context context) throws IOException, InterruptedException
+		{
+			int sum = 0;
+			for(IntWritable val : values)
+			{
+				sum += val.get();
+			}
+			result.set(sum);
+			context.write(key, result);
+		}
+	}
+
+	public static void main(String[] args) throws Exception
+	{
+		Configuration conf = new Configuration();
+		Job job = new Job(conf, "imdb");
+
+		job.setJarByClass(IMDb.class);
+		job.setMapperClass(IMDbMapper.class);
+		job.setReducerClass(IMDbReducer.class);
+
+		job.setOutputKeyClass(Text.class);
+		job.setOutputValueClass(IntWritable.class);
+
+		job.setInputFormatClass(TextInputFormat.class);
+		job.setOutputFormatClass(TextOutputFormat.class);
+
+		FileInputFormat.addInputPath(job, new Path(args[0]));
+		FileOutputFormat.setOutputPath(job, new Path(args[1]));
+		FileSystem.get(job.getConfiguration()).delete(new Path(args[1]), true);
+		job.waitForCompletion(true);
+	}
+}
