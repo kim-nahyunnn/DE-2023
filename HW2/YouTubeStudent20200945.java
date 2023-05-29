f7e1592e59d174e6aa3c98e41679bec388203109
kim-nahyunnn <nahyun8701@gmail.com>
2023-05-29 18:12:56 +0900

    HW2

---
+++ HW2/YouTubeStudent20200945.java
@@ -0,0 +1,134 @@
+import java.io.IOException;
+import java.util.*;
+
+import org.apache.hadoop.conf.*;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.*;
+import org.apache.hadoop.mapreduce.*;
+import org.apache.hadoop.mapreduce.lib.input.*;
+import org.apache.hadoop.mapreduce.lib.output.*;
+import org.apache.hadoop.util.GenericOptionsParser;
+import org.apache.hadoop.fs.FSDataInputStream;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class YouTubeStudent20200945{
+
+	public class Youtube{
+		public String category;
+		public float rating;
+		
+		public Youtube(String category, float rating){
+			this.category = category;
+			this.rating = rating;
+		}
+		
+		public String getString(){
+			return category + " " + rating;
+		}
+	}
+	
+	public static class YoutubeComparator implements Comparator<Youtube>{
+		public int compare(Youtube x, Youtube y){
+			if(x.rating > y.rating) return 1;
+			if(x.rating < y.rating) return -1;
+			return 0;
+		}
+	}
+	
+	public static void insertYoutube(PriorityQueue q, String category, float rating, int topK){
+		Youtube youtube_head = (Youtube) q.peek;
+		if(q.size() < topK || youtube_head.rating < rating){
+			Youtube youtube = new youtube(category, rating);
+			q.add(youtube);
+			if(q.size() > topK) q.remove();
+		}
+	}
+	
+	public static class YoutubeMapper extends Mapper<Object, Text, Text, FloatWritable>{
+		private FloatWritable rslt = new FloatWritable();
+		private Text word = new Text();
+	
+		public void map(Object key, Text value, Context context) throws IOException, InterruptedException{
+			StringTokenizer itr = new StringTokenizer(value.toString(), "|");
+			String tong = itr.nextToken();
+			tong = itr.nextToken();
+			tong = itr.nextToken();
+			word.set(itr.nextToken());
+			
+			tong = itr.nextToken();
+			tong = itr.nextToken();
+			float rating = Float.parseFloat(itr.nextToken());
+			rslt.set(rating);
+			
+			context.write(word, rslt);
+		}
+	}
+	
+	public static class YoutubeReduce extends Reducer<Text, FloatWritable, Text, NullWritable>{
+		private PriorityQueue<Youtube> queue;
+		private Comparator<Youtube> comp = new YoutubeComparator();
+		private int topK;
+		
+		public void reduce(Text key, Iterable<FloatWritable> values, Context context) throws IOException, InterruptedException{
+			float sum = 0;
+			int count = 0;
+			for(FloatWritable val : values){
+				count += 1;
+				sum += val.get();
+			}
+			float rslt = sum / count;
+			String category = key.get();
+			insertYoutube(queue, category, rslt, topK);
+		}	
+		
+		protected void setup(Context context) throws IOException, InterruptedException{
+			Configuration conf = context.getConfiguration();
+			topK = conf.getInt("topK", -1);
+			queue = new PriorityQueue<Youtube>(topK, comp);
+		}
+		
+		protected void cleanup(Context context) throws IOException, InterruptedException{
+			while(queue.size() != 0){
+				Youtube yt = (Youtube)queue.remove();
+				context.write(new Text(yt.getString()), NullWritable.get());
+			}
+		}
+	}
+	
+	public static void main(String[] args) throws Exception{
+		Configuration conf = new Configuration();
+		String[] otherArgs = new GenericOptionsParser(conf, args).getRemainingArgs();
+		int topK = Integer.parseInt(new Path(otherArgs[2]));
+		
+		conf.setInt("topK", topK);
+		Job job = new Job(conf, "TopK");
+		job.setJarByClass(YouTubeStudent20200945.class);
+		job.setMapperClass(YoutubeMapper.class);
+		job.setReducerClass(YoutubeReducer.class);
+
+		job.setOutputKeyClass(Text.class);
+		job.setOutputValueClass(NullWritable.class);
+
+		job.setInputFormatClass(TextInputFormat.class);
+		job.setOutputFormatClass(TextOutputFormat.class);
+
+		FileInputFormat.addInputPath(job, new Path(args[0]));
+		FileOutputFormat.setOutputPath(job, new Path(args[1]));
+		FileSystem.get(job.getConfiguration()).delete(new Path(args[1]), true);
+		job.waitForCompletion(true);
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
